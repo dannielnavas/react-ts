0010c127a32595f8d4f95fb4ff89c2ed1c7328f3
Danniel Navas <dannielnavas@gmail.com>
2024-07-29 07:35:21 -0500

    ✨ feat: Update App component to use lazy loading for images

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,30 @@ import { useState } from "react";
 import "./App.css";
 import { random, RandomFox } from "./components/RandomFox";
 
+type ImageItems = {
+  id: number;
+  url: string;
+};
+
 function App() {
-  const [images, setImages] = useState<string[]>([
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
+  const [images, setImages] = useState<ImageItems[]>([
+    { id: 1, url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: 2, url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: 3, url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: 4, url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: 5, url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: 6, url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: 7, url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: 8, url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: 9, url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: 10, url: `https://randomfox.ca/images/${random()}.jpg` },
   ]);
 
   return (
     <>
       <div className="flex gap-2 flex-row flex-wrap">
-        {images.map((image, index) => (
-          <RandomFox key={index} image={image} />
+        {images.map(({ id, url }) => (
+          <RandomFox key={id} image={url} />
         ))}
       </div>
     </>
