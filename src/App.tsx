3c6906f8ce880ea439531888c2d441a4a558f75c
Danniel Navas <dannielnavas@gmail.com>
2024-07-29 07:29:35 -0500

    ✨ feat: Update App component to use lazy loading for images

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import { random, RandomFox } from "./components/RandomFox";
 
 function App() {
-  const image = `https://randomfox.ca/images/${random()}.jpg`;
+  const [images, setImages] = useState<string[]>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
+
   return (
     <>
-      <RandomFox image={image} />
+      <div className="flex gap-2 flex-row flex-wrap">
+        {images.map((image, index) => (
+          <RandomFox key={index} image={image} />
+        ))}
+      </div>
     </>
   );
 }
