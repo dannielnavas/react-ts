f583ae37139e52d4bc07e1cff0491c289ceac5c2
Danniel Navas <dannielnavas@gmail.com>
2024-07-29 07:48:57 -0500

    ✨ feat: Add button to dynamically add images in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import type { MouseEventHandler } from "react";
 import { useState } from "react";
 import "./App.css";
 import { random, RandomFox } from "./components/RandomFox";
@@ -8,21 +9,25 @@ type ImageItems = {
 };
 
 function App() {
-  const [images, setImages] = useState<ImageItems[]>([
-    { id: 1, url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: 2, url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: 3, url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: 4, url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: 5, url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: 6, url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: 7, url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: 8, url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: 9, url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: 10, url: `https://randomfox.ca/images/${random()}.jpg` },
-  ]);
+  const [images, setImages] = useState<ImageItems[]>([]);
+
+  const addNewImage: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+    const newImage: ImageItems = {
+      id: images.length + 1,
+      url: `https://randomfox.ca/images/${random()}.jpg`,
+    };
+    setImages([...images, newImage]);
+  };
 
   return (
     <>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-8"
+        onClick={addNewImage}
+      >
+        Add image
+      </button>
       <div className="flex gap-2 flex-row flex-wrap justify-center">
         {images.map(({ id, url }) => (
           <RandomFox key={id} image={url} />
