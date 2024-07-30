168a99af105d02f82ec6dca40c166ec50045c80c
Danniel Navas <dannielnavas@gmail.com>
2024-07-30 07:21:56 -0500

    ✨ feat: Add lazy loading for images in RandomFox component

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,6 +1,6 @@
 // import type { FC, FunctionComponent } from "react";
 
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 // export const RandomFoxImpl = () => {
 //   return <img src="https://randomfox.ca/images/1.jpg" alt="Random fox" />;
@@ -18,13 +18,32 @@ type RandomFoxProps = {
 
 export const RandomFox = ({ image, alt }: RandomFoxProps): JSX.Element => {
   const node = useRef<HTMLImageElement>(null); // null es el valor inicial siempre inicializar para evitar errores
+  const [src, setSrc] = useState(
+    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+  );
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log("Intersecting");
+          setSrc(image);
+        }
+      });
+    });
+
+    if (node.current) observer.observe(node.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [image]);
 
   return (
     <img
       ref={node}
-      src={image}
+      src={src}
       alt={alt || "Random fox"}
-      className="rounded-lg h-80 w-80 aspect-video"
+      className="rounded-lg h-80 w-80 aspect-video bg-gray-500"
     />
   );
 };
